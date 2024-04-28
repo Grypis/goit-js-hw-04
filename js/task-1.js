e1fb7dc68416bfc493f66fbde7d5972284be91aa
Grypis <grypisnecros@gmail.com>
2024-04-28 21:31:51 +0300

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+'use strict';
+function isEnoughCapacity(products, containerSize) {
+  let productQuantity = 0;
+  for (const value of Object.values(products)) {
+    productQuantity += value;
+  }
+  return productQuantity <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
