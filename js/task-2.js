3bf17403c3389a48a396255024a07e47ad817d0a
Grypis <grypisnecros@gmail.com>
2024-04-28 23:48:08 +0300

    done

---
+++ js/task-2.js
@@ -0,0 +1,37 @@
+'use strict';
+function calcAverageCalories(days) {
+  let totalCaloriesPerDay = 0;
+  for (const caloriesPerDay of days) {
+    totalCaloriesPerDay += caloriesPerDay.calories;
+  }
+  if (days.length !== 0) {
+    return totalCaloriesPerDay / days.length;
+  }
+  return 0;
+}
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 3010 },
+    { day: 'tuesday', calories: 3200 },
+    { day: 'wednesday', calories: 3120 },
+    { day: 'thursday', calories: 2900 },
+    { day: 'friday', calories: 3450 },
+    { day: 'saturday', calories: 3280 },
+    { day: 'sunday', calories: 3300 },
+  ]),
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 2040 },
+    { day: 'tuesday', calories: 2270 },
+    { day: 'wednesday', calories: 2420 },
+    { day: 'thursday', calories: 1900 },
+    { day: 'friday', calories: 2370 },
+    { day: 'saturday', calories: 2280 },
+    { day: 'sunday', calories: 2610 },
+  ]),
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
